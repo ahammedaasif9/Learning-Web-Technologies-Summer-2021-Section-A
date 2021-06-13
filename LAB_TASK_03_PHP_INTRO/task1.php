ae6a034c59ed61529df75383d2524ee128a22ccc
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-13 10:53:48 +0600

    Done

---
+++ LAB_TASK_03_PHP_INTRO/task1.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    
+<body>
+<?php  
+ $length = 14;  
+ $width = 12;  
+ echo "area of rectangle is $length * $width= " . ($length * $width) . "<br />"; 
+ echo "perimeter of rectangle is $length * $width= " . (2*($length+$width)) . "<br />"; 
+	
+  ?>  
+</body>
+</html>
