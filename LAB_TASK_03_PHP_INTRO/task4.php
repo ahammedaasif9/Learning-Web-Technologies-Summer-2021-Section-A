b7a02a994cd7c104f462ce475a39ef229621487d
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-13 10:37:40 +0600

    Done

---
+++ LAB_TASK_03_PHP_INTRO/task4.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+$a = 40;
+$b = 25;
+$c = 7;
+
+echo "The numbers are a = $a, b = $b, c = $c <br>";
+if(($a > $b) && ($a > $c)) 
+ echo "a = $a is the biggest number";
+
+else if($b > $c) 
+ echo "b = $b is the biggest number";
+
+else
+ echo "c = $c is the biggest number";
+?>
+</body>
