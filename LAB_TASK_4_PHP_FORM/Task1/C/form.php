303b46d0623cb54a89723636fc88940594c44419
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-15 19:34:12 +0600

    Done

---
+++ LAB_TASK_4_PHP_FORM/Task1/C/form.php
@@ -0,0 +1,26 @@
+<?php
+	
+	if(isset($_REQUEST['submit'])){
+		
+		$email = $_REQUEST['email'];
+
+		if($email == ""){
+			echo "invalid email!";
+		}else{
+			echo $email;
+		}
+
+	}else{
+		echo 'invalid request';
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+	<form method="REQUEST" action="email.php">
+		<fieldset><legend>Email</legend> <input type="email" name="email" value="" /><br>
+				<input type="submit" name="submit" value="Submit"></fieldset>
+	</form>
+</body>
+</html>
