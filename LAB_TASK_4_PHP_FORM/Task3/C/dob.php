3b711e75c2e708d054cc80d2cb8963c0880275d1
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-16 11:44:07 +0600

    Done

---
+++ LAB_TASK_4_PHP_FORM/Task3/C/dob.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			<legend>DATE OF BIRTH</legend>
+			<input type="date" name="dob" value="<?php if(isset($_POST['dob'])){ echo $_POST['dob']; }?>"> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body> 
+</html>
