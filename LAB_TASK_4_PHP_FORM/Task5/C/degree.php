b15df8e8c3eac9e959cc5cd25db15950469dd878
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-16 12:02:55 +0600

    Done

---
+++ LAB_TASK_4_PHP_FORM/Task5/C/degree.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			<legend>DEGREE</legend>
+			<input type="checkbox" name="degree"> SSC
+			<input type="checkbox" name="degree"> HSC
+			<input type="checkbox" name="degree"> BSc 
+            <input type="checkbox" name="degree"> Msc <br> value="<?php if(isset($_POST['degree'])){ echo $_POST['degree']; }?>"> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form> 
+</body>
+</html>
