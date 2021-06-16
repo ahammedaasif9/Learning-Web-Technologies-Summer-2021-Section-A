bfe8f46cf46857e46e0dc2d4ce5624aa416845e1
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-16 11:50:21 +0600

    Done

---
+++ LAB_TASK_4_PHP_FORM/Task4/C/gender.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			<legend>GENDER</legend>
+			<input type="radio" name="gender"> Male
+			<input type="radio" name="gender"> Female
+			<input type="radio" name="gender"> Other <br> value="<?php if(isset($_POST['gender'])){ echo $_POST['gender']; }?>"> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html> 
