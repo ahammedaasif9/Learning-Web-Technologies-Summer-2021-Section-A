994e88454387f65e1d1b816875b9c950a97c9347
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-16 12:10:38 +0600

    Done

---
+++ LAB_TASK_4_PHP_FORM/Task6/C/blood_group.php
@@ -0,0 +1,23 @@
+  
+<!DOCTYPE html>
+<html>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			<legend>Blood Group</legend>
+            <select name="" id="">
+                <option value="bloodgroup"></option>
+                <option value="bloodgroup">A+</option>
+                <option value="bloodgroup">A-</option>
+                <option value="bloodgroup">B-</option>
+                <option value="bloodgroup">B+</option>
+                <option value="bloodgroup">O-</option>
+                <option value="bloodgroup">O+</option>
+                
+
+            </select> <br> value="<?php if(isset($_POST['bloodgroup'])){ echo $_POST['bloodgroup']; }?>"> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html> 
