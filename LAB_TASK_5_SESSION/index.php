fa2d7016db3365ea7444a421b577fa258528d8c5
Ahammed Asif <44169137+ahammedaasif9@users.noreply.github.com>
2021-06-20 10:44:22 +0600

    Index Page

---
+++ LAB_TASK_5_SESSION/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <center>
+    <table border="1">
+
+        <tr>
+            <td align="Left">
+                <h3><b>Xcompany</b></h3>
+            </td>
+
+            <td align="Right">
+                <a href="view/Home.php">Home</a> |
+                <a href="view/Login.php">Login</a> |
+                <a href="view/Registation.php">Registation</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td align="Left" colspan="2">
+                <b>Welcome to Xcompany<b>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <center>
+                Copyright © 2017
+                </center>
+            </td>
+        </tr>
+    </table>
+    </center>
+</body>
+
+</html>
