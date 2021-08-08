29ae6df381e97f82b00adaefba87079bf5d1f3e4
ASIF AL JUBAYER RIFAT <44169137+ahammedaasif9@users.noreply.github.com>
2021-08-08 10:55:23 +0600

    Done

---
+++ WT_LAB_FINAL/jobportal.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2021 at 06:50 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jobportal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('rohim', '12'),
+('a', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `sl` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `cname` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`sl`, `name`, `cname`, `phone`, `username`, `password`) VALUES
+(1, 'hakim', 'hub', '0987654323', 'hakim12', '12'),
+(2, 'a', 'a', 'a', 'e', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `joblist`
+--
+
+CREATE TABLE `joblist` (
+  `sl` int(11) NOT NULL,
+  `comname` varchar(110) NOT NULL,
+  `jobtitle` varchar(11) NOT NULL,
+  `location` varchar(110) NOT NULL,
+  `salary` varchar(110) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `joblist`
+--
+
+INSERT INTO `joblist` (`sl`, `comname`, `jobtitle`, `location`, `salary`) VALUES
+(1, 't', 't', 't', 't');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`sl`);
+
+--
+-- Indexes for table `joblist`
+--
+ALTER TABLE `joblist`
+  ADD PRIMARY KEY (`sl`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `joblist`
+--
+ALTER TABLE `joblist`
+  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
